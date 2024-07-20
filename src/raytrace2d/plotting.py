369ce6cdb34d23586acad8afe78fbe31cb9ff36f
William Jenkins <wfjenkins@ucsd.edu>
2024-07-20 00:34:03 -0400

    Add plotting functions

--- src/raytrace2d/plotting.py
+++ src/raytrace2d/plotting.py
@@ -14,8 +14,17 @@ class RayTrace(Protocol):
     def plot(self): ...
 
 
-class SoundSpeedProfile(Protocol):
-    def plot(self): ...
+def plot_bathymetry(
+    distance: np.ndarray,
+    water_depth: np.ndarray,
+    ax: Optional[plt.Axes] = None,
+    *args,
+    **kwargs
+) -> plt.Axes:
+    if ax is None:
+        ax = plt.gca()
+    ax.plot(distance, water_depth, *args, **kwargs)
+    return ax
 
 
 def plot_ray(
@@ -29,6 +38,48 @@ def plot_ray(
         ax = plt.gca()
     ax.plot(distance, depth, *args, **kwargs)
     return ax
+
+
+def plot_ray_trace(raytrace: RayTrace) -> plt.Figure:
+
+    FIG_KW = {
+        "figsize": (12, 4),
+        "nrows": 1,
+        "ncols": 2,
+        "gridspec_kw": {"width_ratios": [1, 4], "wspace": 0.0},
+    }
+
+    SSP_KW = {
+        "c": "k",
+    }
+
+    RAY_KW = {
+        "c": "k",
+    }
+    max_depth = raytrace.bathymetry.water_depth.max()
+    zvec = np.linspace(0, max_depth, 1001)
+
+    fig, axs = plt.subplots(**FIG_KW)
+
+    ax = axs[0]
+    ax = plot_ssp(1 / raytrace.profile(zvec), zvec, ax=ax, **SSP_KW)
+    # ax = raytrace.profile.plot(ax=ax)
+    ax.set_ylim(0, max_depth)
+    ax.invert_yaxis()
+    ax.set_xlabel("Speed [m/s]")
+    ax.set_ylabel("Depth [m]")
+    ax.tick_params(top=True, labeltop=True, bottom=False, labelbottom=False)
+
+    ax = axs[1]
+    ax = plot_rays(raytrace.rays, ax=ax, **RAY_KW)
+    ax = plot_bathymetry(raytrace.bathymetry.distance, raytrace.bathymetry.water_depth, ax=ax)
+    ax.set_xlim(0, max(raytrace.bathymetry.distance))
+    ax.set_ylim(0, max_depth)
+    ax.invert_yaxis()
+    ax.set_yticklabels([])
+    ax.set_xlabel("Range [m]")
+
+    return fig
 
 
 def plot_rays(
@@ -48,6 +99,3 @@ def plot_ssp(
         ax = plt.gca()
     ax.plot(speed, depth, *args, **kwargs)
     return ax
-
-
-# TODO: Plot all of the above in a single figure
